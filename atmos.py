1b4ffc81c664bfe42d7d74d31ec62916aee8feaf
Conor.McManus <Conor.McManus@gfk.com>
2019-04-15 15:43:24 +0200

    Remove unused import

--- atmos.py
+++ atmos.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse, subprocess, shlex, sys, os, glob
-from jinja2 import Environment, FileSystemLoader
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Control Terraform Workspaces.')
